dfd9f88788fb2c8f499f2c76477bd9c91b39b0cb
rahulvrk <rahul.kulkarni@etouch.net>
2018-07-08 12:48:28 +0530

    Test

---
+++ src/test/java/com/testt/com/com/testt/com/GmailTest.java
@@ -0,0 +1,14 @@
+package com.testt.com.com.testt.com;
+
+import org.testng.annotations.Test;
+
+public class GmailTest {
+	// TODO Auto-generated constructor stub
+
+	
+	@Test
+	public void Test1(){
+		System.out.println("ahello");
+	}
+
+}
